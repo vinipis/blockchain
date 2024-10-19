7d869815ff59110d075086c7c5d648249c4757c4
Vinicius <vinicius@senseinode.com>
2024-10-19 16:47:02 -0300

    criação de bocks

--- blockchain-golang/main.go
+++ blockchain-golang/main.go
@@ -1,5 +1,56 @@
 package main
 
-func main() {
+import (
+	"bytes"
+	"crypto/sha256"
+	"fmt"
+)
 
+type Blockchain struct {
+	blocks []*Block
+}
+type Block struct {
+	Hash     []byte
+	Data     []byte
+	PrevHash []byte
+}
+
+func (b *Block) DeriveHash() {
+	info := bytes.Join([][]byte{b.Data, b.PrevHash}, []byte{})
+	hash := sha256.Sum256(info)
+	b.Hash = hash[:]
+}
+
+func CreateBlock(data string, prevHash []byte) *Block {
+	block := &Block{[]byte{}, []byte(data), prevHash}
+	block.DeriveHash()
+	return block
+}
+
+func (chain *Blockchain) AddBlock(data string) {
+	prevBlock := chain.blocks[len(chain.blocks)-1]
+	new := CreateBlock(data, prevBlock.Hash)
+	chain.blocks = append(chain.blocks, new)
+}
+
+func Genesis() *Block {
+	return CreateBlock("Genesis", []byte{})
+}
+
+func InitBlockchain() *Blockchain {
+	return &Blockchain{[]*Block{Genesis()}}
+}
+
+func main() {
+	chain := InitBlockchain()
+
+	chain.AddBlock("First Block After Genesis")
+	chain.AddBlock("Second Block After Genesis")
+	chain.AddBlock("Third Block After Genesis")
+
+	for _, block := range chain.blocks {
+		fmt.Printf("Previous Hash: %x\n", block.PrevHash)
+		fmt.Printf("Data in block: %s\n", block.Data)
+		fmt.Printf("Hash: %x\n", block.Hash)
+	}
 }
