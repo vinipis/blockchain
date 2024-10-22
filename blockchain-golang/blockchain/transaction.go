2bb4469688ba0ea38034f3e29a9e565e53a26941
Vinicius <vinicius@senseinode.com>
2024-10-22 15:36:53 -0300

    refatoramento

--- blockchain-golang/blockchain/transaction.go
+++ blockchain-golang/blockchain/transaction.go
@@ -15,17 +15,6 @@ type Transaction struct {
 	Outputs []TxOutput
 }
 
-type TxOutput struct {
-	Value  int
-	PubKey string
-}
-
-type TxInput struct {
-	ID  []byte
-	Out int
-	Sig string
-}
-
 func (tx *Transaction) SetId() {
 	var encoded bytes.Buffer
 	var hash [32]byte
@@ -86,12 +75,4 @@ func NewTransaction(from string, to string, amount int, chain *Blockchain) *Tran
 
 func (tx *Transaction) IsCoinBase() bool {
 	return len(tx.Inputs) == 1 && len(tx.Inputs[0].ID) == 0 && tx.Inputs[0].Out == -1
-}
-
-func (in *TxInput) CanUnlock(data string) bool {
-	return in.Sig == data
-}
-
-func (out *TxOutput) CanBeUnlocked(data string) bool {
-	return out.PubKey == data
 }
